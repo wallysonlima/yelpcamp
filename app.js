397ba93866609e889f2bbf864af4ab9639fbc543
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-09-29 13:06:40 -0300

    Create form to add new comments

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ var express 	= require("express"),
   Campground = require("./models/campground"),
   seedDB = require("./seeds");
 
-  seedDB();
-
 mongoose.connect("mongodb://localhost:27017/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+seedDB();
 
 app.get("/", function(req, res){
   res.render("landing");
@@ -21,7 +20,7 @@ app.get("/campgrounds", function(req, res){
   	if(err){
   		console.log(err);
   	} else {
-  		res.render("index", {campgrounds:allCampgrounds});
+  		res.render("campgrounds/index", {campgrounds:allCampgrounds});
   	}
   });
   //res.render("campgrounds", {campgrounds:campgrounds});
@@ -46,7 +45,7 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/new", function(req, res){
-  res.render("new.ejs");
+  res.render("campgrounds/new");
 });
 
 // SHOW - shows more info about one campground
@@ -57,12 +56,28 @@ app.get("/campgrounds/:id", function(req, res){
   		console.log(err);
   	} else {
   		// render show template with that campground
-  		res.render("show", {campground: foundCampground});		
+  		res.render("campgrounds/show", {campground: foundCampground});		
   	}
   });
   req.params.id;
   
 });
+
+// ======================
+// COMMENTS ROUTES
+// ======================
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+  // find campground by id
+  Campground.findById(req.params.id, function(err, campground){
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
 
 app.listen(3000, function(){
   console.log("The YelpCamp Server has started !!!");
