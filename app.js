dc37d376d4185552297a80abd2fc6d4b9195efed
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-09-29 12:19:59 -0300

    Add comments to the campgrounds

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
 var express 	= require("express"),
 	app 		= express(),
 	bodyParser 	= require("body-parser"),
-	mongoose 	= require("mongoose");
-  Campground = require("./models/campground");
+	mongoose 	= require("mongoose"),
+  Campground = require("./models/campground"),
+  seedDB = require("./seeds");
+
+  seedDB();
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-
-/*Campground.create(
-	{
-		name: "Granite Hill",
-		image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg",
-		description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite !"
-	}, function(err, campground){
-		if(err) {
-			console.log(err);
-		} else {
-			console.log("Newly create campground !");
-			console.log(campground);
-		}
-	}
-);
-
-
- /*var campgrounds = [
-    {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2014/05/03/00/42/vw-camper-336606__340.jpg"},
-    {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906__340.jpg"}
-  ];
-  */
 app.get("/", function(req, res){
   res.render("landing");
 });
@@ -72,7 +52,7 @@ app.get("/campgrounds/new", function(req, res){
 // SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res){
   // find the campground with provided ID
-  Campground.findById(req.params.id, function(err, foundCampground){
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
   	if(err) {
   		console.log(err);
   	} else {
