2b32010cab13a54725cd9d240e8577fdd69cf389
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-10-01 15:04:12 -0300

    Finished the comment functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express 	= require("express"),
 	bodyParser 	= require("body-parser"),
 	mongoose 	= require("mongoose"),
   Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
   seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp");
@@ -76,6 +77,30 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
       res.render("comments/new", {campground: campground});
     }
   });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+  // lookup campground using ID
+  Campground.findById(req.params.id, function(err, campground){
+    if(err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, function(err, comment){
+        if(err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect('/campgrounds/' + campground._id);
+        }
+        
+      });
+    }
+  });
+  // create new comment
+  // connect new comment to campground
+  //redirect campground show page
 });
 
 
