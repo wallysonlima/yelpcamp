d5ff41807e420556d8569d7153dc3e4fa9d4b179
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-10-03 19:39:54 -0300

    add login route

--- app.js
+++ app.js
@@ -143,6 +143,19 @@ app.post("/register", function(req, res){
   });
 });
 
+// show login form
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", 
+  {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), function(req, res){
+    res.send("Login logic happens here!");
+});
+
 app.listen(3000, function(){
   console.log("The YelpCamp Server has started !!!");
 });
