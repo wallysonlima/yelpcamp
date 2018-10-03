c3ade40e9ebc3624938a719c9c0ef4ec9a047a05
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-10-03 20:26:28 -0300

    Add logout route and navbar

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get("/", function(req, res){
   res.render("landing");
 });
@@ -38,7 +43,7 @@ app.get("/campgrounds", function(req, res){
   	if(err){
   		console.log(err);
   	} else {
-  		res.render("campgrounds/index", {campgrounds:allCampgrounds});
+  		res.render("campgrounds/index", {campgrounds:allCampgrounds, currentUser: req.user});
   	}
   });
   //res.render("campgrounds", {campgrounds:campgrounds});
