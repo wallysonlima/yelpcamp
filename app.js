80322b4795017f8f33ca70ac9b13a4808c21b2ae
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-10-03 19:29:57 -0300

    Add register route in the app.js

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express 	= require("express"),
 	app 		= express(),
 	bodyParser 	= require("body-parser"),
 	mongoose 	= require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
   Campground = require("./models/campground"),
   Comment = require("./models/comment"),
+  User = require("./models/user"),
   seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp");
@@ -11,6 +14,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "Camilinha is the best cat ever !",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("landing");
@@ -104,6 +120,28 @@ app.post("/campgrounds/:id/comments", function(req, res){
   //redirect campground show page
 });
 
+// ============
+// Auth Routes
+// ============
+
+// show register form
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+// handle sign up logic
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    });
+  });
+});
 
 app.listen(3000, function(){
   console.log("The YelpCamp Server has started !!!");
