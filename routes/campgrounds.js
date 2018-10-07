2014e798d76f9a2f7061e49da80be93706e6b3b0
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-10-07 17:57:03 -0300

    Create authorization in the page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,18 +57,20 @@ router.get("/:id", function(req, res){
 });
 
  // EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", function(req, res){
-  Campground.findById(req.params.id, function(err, found){
-    if(err){
-      res.redirect("/campgrounds")
-    } else {
-      res.render("campgrounds/edit", {campground: foundCampground});
-    }
-  });
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
+  // is user logged in
+ 
+    Campground.findById(req.params.id, function(err, found){
+        res.render("campgrounds/edit", {campground: foundCampground});  
+    });
+  // does user own the campground ?
+  // otherwise, redirect
+  // if not, redirect
+  
 });
 
 // UPDATE CAMPGROUND ROUTE
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
   // find and update the correct campground
   Campground.findByIdAndUpdate(req.params.id, req.params.campground, function(err, updatedCampground){
     if (err) {
@@ -81,7 +83,7 @@ router.put("/:id", function(req, res){
 });
 
 // DESTROY CAMPGROUND ROUTE
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
   Campground.findByIdAndRemove(req.params.id, function(err){
     if(err){
       res.redirect("/campgrounds");
@@ -91,6 +93,27 @@ router.delete("/:id", function(req, res){
   });
 });
 
+
+function checkCampgroundOwnership(req, res, next) {
+  if(req.isAuthenticated()){
+    Campground.findById(req.params.id, function(err, found){
+      if(err){
+        res.redirect("back");
+      } else {
+        // does user own the campground?
+        if(foundCampground.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+        
+      }
+      });
+  } else {
+    
+    res.redirect("back");
+  }
+}
 
 // middleware
 function isLoggedIn(req, res, next){
